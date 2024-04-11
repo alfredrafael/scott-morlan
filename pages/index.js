83140e5f8f5c9591b6b797841357bc18c41e71a7
Alfredo R. Pabon <alfredrafael@gmail.com>
2024-04-11 14:47:01 -0500

    Update title tab

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>ScottMorlan.com</title>
         <link
           rel="icon"
           href="http://www.alfredorafael.com/wp-content/uploads/2019/02/myLogo.png"
